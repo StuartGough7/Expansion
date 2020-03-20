7da0a899c51ae53d363927d6954a75d02fb02182
Stuart Gough <stuartgough7@gmail.com>
2020-03-20 23:27:03 +0000

    Basic Tile change on click

---
+++ Assets/Scripts/TileClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileClick : MonoBehaviour
+{
+  public Tilemap tileMap;
+  public TileBase swappedTile;
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      Vector3 position = Input.mousePosition;
+      Vector3Int tilePos = tileMap.WorldToCell(Camera.main.ScreenToWorldPoint(position));
+      tileMap.SetTile(tilePos, swappedTile);
+    }
+  }
+}
